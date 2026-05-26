e00566a05bf5f3c905788588a85fd843db8b2fb2
MrTouchpad <144840383+MrTouchpad@users.noreply.github.com>
2026-05-27 00:10:44 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,45 @@
-// Copyright 2021 NNTU-CS
+// Copyright 2022 NNTU-CS
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
 #include "train.h"
 
-Train::Train() : countOp(0), first(nullptr) {}
-
-void Train::addCar(bool light) {
-  Car *car = new Car{light, nullptr, nullptr};
-  if (!first) {
-    car->next = car;
-    car->prev = car;
-    first = car;
-  } else {
-    Car *last = first->prev;
-    last->next = car;
-    car->prev = last;
-    car->next = first;
-    first->prev = car;
+int main() {
+  Train train;
+  int count = 60;
+  std::srand(time(nullptr));
+  while (count--)
+    train.addCar(false);
+  std::cout << "n\tfalse\t\ttrue\t\trndom\n";
+  for (int n = 2; n <= 100; n++) {
+    int opFalse;
+    int opTrue;
+    int opRandom;
+    {
+      Train t;
+      for (int i = 0; i < n; i++)
+        t.addCar(false);
+      t.getLength();
+      opFalse = t.getOpCount();
+    }
+    {
+      Train t;
+      for (int i = 0; i < n; i++)
+        t.addCar(true);
+      t.getLength();
+      opTrue = t.getOpCount();
+    }
+    {
+      Train t;
+      for (int i = 0; i < n; i++)
+        t.addCar(std::rand() % 2);
+      t.getLength();
+      opRandom = t.getOpCount();
+    }
+    std::cout << n << "\t" << opFalse << "\t\t" << opTrue << "\t\t"
+              << opRandom << "\n";
   }
-}
-
-int Train::getLength() {
-  if (!first) return 0;
-  bool startWasOn = first->light;
-  first->light = false;
-  Car *cur = first->next;
-  int len = 1;
-  while (cur != first) {
-    countOp++;
-    if (!cur->light)
-      cur->light = true;
-    len++;
-    cur = cur->next;
-  }
-  first->light = startWasOn;
-  return len;
-}
-
-int Train::getOpCount() {
-  return countOp;
+  std::cout << train.getLength() << std::endl;
+  std::cout << train.getOpCount() << std::endl;
+  return 0;
 }
