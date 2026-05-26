138a99379d1661f72bd39de4e1e5a78bff1637ce
MrTouchpad <144840383+MrTouchpad@users.noreply.github.com>
2026-05-27 00:16:10 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,45 +1,39 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
-
-Train::Train() : countOp(0), first(nullptr) {}
-
+Train::Train() : first(nullptr), countOp(0) {}
+int Train::getOpCount() { return countOp; }
 void Train::addCar(bool light) {
-  Car *car = new Car{light, nullptr, nullptr};
-  if (!first) {
-    car->next = car;
-    car->prev = car;
-    first = car;
-  } else {
-    Car *last = first->prev;
-    last->next = car;
-    car->prev = last;
-    car->next = first;
-    first->prev = car;
+  Car *newCar = new Car;
+  newCar->light = light;
+  if (first == nullptr) {
+    first = newCar;
+    newCar->next = newCar;
+    newCar->prev = newCar;
+    return;
   }
+  Car *last = first->prev;
+  newCar->next = first;
+  newCar->prev = last;
+  last->next = newCar;
+  first->prev = newCar;
 }
-
 int Train::getLength() {
-  if (!first) return 0;
-  bool startWasOn = first->light;
-  first->light = false;
-  Car *cur = first;
-  int len = 1;
-  while (cur->next != first) {
-    countOp++;
-    cur = cur->next;
-    if (!cur->light)
-      cur->light = true;
-    len++;
+  Car *currentCar = first;
+  currentCar->light = true;
+  while (true) {
+    int steps = 0;
+    do {
+      currentCar = currentCar->next;
+      countOp++;
+      steps++;
+    } while (!currentCar->light);
+    currentCar->light = false;
+    for (int i = 0; i < steps; i++) {
+      currentCar = currentCar->prev;
+      countOp++;
+    }
+    if (!currentCar->light) {
+      return steps;
+    }
   }
-  cur = first;
-  while (cur->next->light) {
-    countOp++;
-    cur = cur->next;
-  }
-  first->light = startWasOn;
-  return len;
-}
-
-int Train::getOpCount() {
-  return countOp;
 }
